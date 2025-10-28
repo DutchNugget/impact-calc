57431470823e58fa676c6d1f0181cdca9e33e7d9
DutchNugget <232434153+DutchNugget@users.noreply.github.com>
2025-10-28 20:04:23 +0100

    added formhandling function long function jesus christ declared a fuckton of variables calculates all we want now we just need to display results

--- impact-calc.js
+++ impact-calc.js
@@ -1,24 +1,16 @@
 
-const responseRates = {
-  drivers: {
-    A: [0.01, 0.02], //<3 days response times & no input required
-    B: [0.05, 0.07], //>3 Days response times & no input required
-    C: [0.03, 0.05], //<3 days response times & input required
-    D: [0.08, 0.12]  //>3 days response times & input required
-  },
-  customers: {
-    A: [0.02, 0.03], //<3 days response times & no input required
-    B: [0.08, 0.10], //>3 Days response times & no input required
-    C: [0.04, 0.06], //<3 days response times & input required
-    D: [0.10, 0.14]  //>3 days response times & input required
-  }
-};
+const driversRates          = { A: 0.015, B: 0.06, C: 0.04, D: 0.10 };
+const customersRates        = { A: 0.025, B: 0.09, C: 0.05, D: 0.12 };
+const handlingTimeDrivers   = 500;
+const handlingTimeCustomers = 1200;
 
 //selecting all elements that will be needed later 
-const form = document.querySelector("form");
-const userType = document.getElementsByName("userType");
-const amountInput = document.querySelector(`input[name = "amount"]`)
-const resultContainer = document.createElement("div")
+const form            = document.querySelector("form");
+const userType        = document.getElementsByName("userType");
+const amountInput     = document.querySelector(`input[name = "amount"]`)
+
+const effectOr        = document.getElementsByName("effectOr")
+const responseTime    = document.getElementsByName("responseTime")
 
 
 // below are placeholders for when both segments are targeted
@@ -26,6 +18,12 @@ const resultContainer = document.createElement("div")
 let amountDriversInput;   
 let amountCustomersInput;
 const bothContainer = document.createElement("div")
+
+const resultContainer = document.createElement("div")
+resultContainer.style.display = "none"
+resultContainer.classList.add("result");
+form.appendChild(resultContainer);
+
 
 // Converts user type to array for use in later function and adds listener to each radio
 Array.from(userType).forEach(radio => {
@@ -65,9 +63,7 @@ function handleUserType () {
             amountCustomersInput = customerInput;
 
             //insert into DOM 
-                if (!bothContainer.parentNode){
-                    form.insertBefore(bothContainer,amountInput.nextSibling)
-                }
+             amountInput.after(bothContainer);
             } 
             //if not both are selected, remove container
         } else {
@@ -79,3 +75,67 @@ function handleUserType () {
           amountCustomersInput = null;
         }
 };
+
+
+const submitButton = document.getElementById("submitAll");
+submitButton.addEventListener("click",formHandler);
+
+function formHandler (event) {
+    event.preventDefault();
+
+    //get values from inputs used in calculation
+    const selection = Array.from(userType).find(r => r.checked).value;
+    const inputReq  = Array.from(effectOr).find(r => r.checked).value;
+    const backlogg  = Array.from(responseTime).find(r => r.checked).value;
+
+    //get numeric values of amounts
+    let amount         = 0
+    let driversAmount  = 0
+    let customerAmount = 0 
+    if (selection === "customers"){
+        amount = Number(amountInput.value);
+    } else if (selection === "both") {
+        driversAmount  = Number(amountDriversInput.value);
+        customerAmount = Number(amountCustomersInput.value);
+    } else if (selection === "drivers") {
+        amount = Number(amountInput.value);
+    } 
+
+    //figure out scenario 
+        let scenario;
+        if (backlogg === "less3" && inputReq === "no") {
+            scenario = "A";
+        } else if (backlogg === "less3" && inputReq === "yes"){
+            scenario = "C";
+        } else if (backlogg === "more3" && inputReq === "no") {
+            scenario = "B";
+        } else if (backlogg === "more3" && inputReq === "yes") {
+            scenario = "D";
+        } else if (backlogg === "unknown" && inputReq === "yes") {
+            scenario = "D";
+        } else {
+            scenario = "C";
+        }
+
+        //determine rate to calculate number of enquiries
+            let driverRate; 
+            let customerRate;
+            let expectedDriver;
+            let expectedCustomer;
+            let expectedWorkload;
+            if (selection === "drivers"){
+                driverRate       = driversRates[scenario] 
+                expectedDriver   = driverRate * amount
+                expectedWorkload = (expectedDriver * handlingTimeDrivers) / 3600
+            } else if (selection === "customers"){
+                customerRate     = customersRates[scenario]
+                expectedCustomer = customerRate * amount
+                expectedWorkload = (expectedCustomer * handlingTimeCustomers) / 3600
+            } else {
+                customerRate     = customersRates[scenario]
+                driverRate       = driversRates[scenario]
+                expectedDriver   = driverRate   * driversAmount
+                expectedCustomer = customerRate * customerAmount
+                expectedWorkload = ((expectedDriver * handlingTimeDrivers)+ (expectedCustomer * handlingTimeCustomers)) / 3600
+            }
+}
